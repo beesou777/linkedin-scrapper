134d63daa2d2a0442ba2fb0b2f5e69b1e920d91c
beesou777 <sbeesou@gmail.com>
2026-09-13 12:58:45 +0545

    retopre

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ const config = {
     // 3. Go to Application/Storage tab → Cookies → https://www.linkedin.com
     // 4. Find 'li_at' cookie and copy its value
     // 5. Paste it below (the cookie expires periodically, so update it when you get errors)
-    LI_AT_COOKIE: "AQEDAWM_xr8FLJUFAAABm7duZb4AAAGcGMv2100AvzeXCpL0wnONLkFivtSz6pbLGRenA-faapcnh9QKVjMsPFPeYbWH0DhFkpFPJzWVjCPuqB6xyOhaChLcxsP55p33jkmURRXaefqK8HqSD8cgbAyf",
+    LI_AT_COOKIE: "AQEDAWM_xr8D-290AAABoJlww24AAAGgvX1HblYAW9xkvYPO2BQ-fKftTit22Gftw8_wmWuV6luMt6_WjtVBzzNQPIXLmaOIoye3BTsdqCAnZvRhfnoauGdOQjR5eXXzB96RWFJKeAGkQ0CK4lsNKYhi",
 };
 
 export {
